c53e0f522603131f79d3e5a2245f4cfe56a59360
matias cardoso <mattiascardoso10@gmail.com>
2023-01-23 13:51:21 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="App fondo">
+      <div><img src="https://i.postimg.cc/W37F9Vy3/1.png" className='tamañoImagen' alt="" /></div>
+      <div><img src="https://i.postimg.cc/Jz9k0PJ1/2.png" className='tamañoImagen' alt="" /></div>
+      <div><img src="https://i.postimg.cc/Xq7ZmhMg/3.png" className='tamañoImagen' alt="" /></div>
+      <div><img src="https://i.postimg.cc/ZqpydpF6/4.png" className='tamañoImagen' alt="" /></div>
+      <div><img src="https://i.postimg.cc/XJWBcqQm/5.png" className='tamañoImagen' alt="" /></div>
+    
+    
+    
+    
+    </div>
+    </>
+  );
+}
+
+export default App;
